35d95abc0584f1c7738a00a21f7aed3974978279
MDandersonm <1215kimyj@gmail.com>
2023-02-28 18:57:23 +0900

    변경1

--- demo/src/main/java/com/example/demo/domain/vueTest/controller/VueAxiosTestController.java
+++ demo/src/main/java/com/example/demo/domain/vueTest/controller/VueAxiosTestController.java
@@ -4,7 +4,7 @@ import com.example.demo.domain.vueTest.controller.request.VueRequestTestData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
-@Slf4j
+@Slf4j//ss
 @RestController
 @RequestMapping("/vue/first")
 @CrossOrigin(origins = "http://localhost:8080", allowedHeaders = "*")
